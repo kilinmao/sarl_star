37185a25d01c512cde7bac05162257380a70f9aa
kilin <52024757+kilinmao@users.noreply.github.com>
2021-02-14 19:14:09 +0100

    Update task_generator_node.py

--- navigation/task_generator/scripts/task_generator_node.py
+++ navigation/task_generator/scripts/task_generator_node.py
@@ -47,7 +47,7 @@ class TaskGenerator:
         rospy.loginfo("".join(["="]*80))
 
 #    def check_robot_pos_callback(self, odom_msg:Odometry):
-        def check_robot_pos_callback(self, nav_msgs:Odometry):
+        def check_robot_pos_callback(self, Odometry):
         robot_pos = nav_msgs.pose.pose.position
         robot_x = robot_pos.x
         robot_y = robot_pos.y
