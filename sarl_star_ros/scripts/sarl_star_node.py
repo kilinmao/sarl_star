7dd23415308c681c51f4eba8e17b532f4e9cd4ba
kilin <52024757+kilinmao@users.noreply.github.com>
2021-02-16 13:57:38 +0100

    Update sarl_star_node.py

--- sarl_star_ros/scripts/sarl_star_node.py
+++ sarl_star_ros/scripts/sarl_star_node.py
@@ -116,7 +116,7 @@ class RobotAction(object):
         # self.robot_pose_sub = rospy.Subscriber('/amcl_pose', PoseWithCovarianceStamped, self.update_robot_pos)
         self.robot_odom_sub = rospy.Subscriber('/odom', Odometry, self.robot_vel_on_map_calculator)
         self.people_sub = rospy.Subscriber('/people', People, self.update_humans)
-        self.goal_sub = rospy.Subscriber('/sub_goal', PoseStamped, self.get_goal_on_map)
+        self.goal_sub = rospy.Subscriber('/local_goal', PoseStamped, self.get_goal_on_map)
         self.global_costmap_sub = rospy.Subscriber('/move_base/global_costmap/costmap', OccupancyGrid, self.get_gc)
         
 
